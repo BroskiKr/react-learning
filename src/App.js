4ac695cf03296eedf1d0ed4eedbc83ea819e611f
Andrij Krohmalnyj <krohmalnyj.andr@gmail.com>
2024-02-20 22:25:32 +0200

    qwe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/Ui/MyModal/MyModal";
+import MyButton from "./components/Ui/button/MyButton";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -13,6 +15,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
@@ -21,6 +24,7 @@ function App() {
 
   const [filter, setFilter] = useState({ sort: '', query: '' })
 
+  const [modal, setModal] = useState(false)
 
   const sortedPosts = useMemo(() => {
     if (filter.sort) {
@@ -35,7 +39,10 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 5 }} onClick={() => setModal(true)}>Створити користувача</MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
       <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Пости про js' />
