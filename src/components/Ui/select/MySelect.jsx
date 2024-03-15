01ae2217e0d9b0be83fc8d4e981b06e8f296d0e5
Andrij Krohmalnyj <krohmalnyj.andr@gmail.com>
2024-03-15 19:12:28 +0200

    Final

--- src/components/Ui/select/MySelect.jsx
+++ src/components/Ui/select/MySelect.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const MySelect = ({ options, defaultValue, value, onChange }) => {
+const MySelect = ({ options, defaultValue, value, onChange, style }) => {
   return (
-    <select value={value} onChange={e => onChange(e.target.value)}>
-      <option disabled value=''>{defaultValue}</option>
+    <select style={style} value={value} onChange={e => onChange(e.target.value)}>
+      {defaultValue && <option disabled value=''>{defaultValue}</option>}
       {options.map(option =>
         <option key={option.value} value={option.value}>{option.name}</option>
       )}
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import '../styles/Page.css'
 import PostList from "../components/PostList";
 import PostForm from "../components/PostForm";
@@ -11,6 +11,8 @@ import Loader from "../components/Ui/Loader/Loader";
 import { useFetching } from "../hooks/useFetching";
 import { getPageCount } from "../utils/pages";
 import Pagination from "../components/Ui/Pagination/Pagination";
+import { useObserver } from "../hooks/useObserver";
+import MySelect from "../components/Ui/select/MySelect";
 
 function Posts() {
   const [posts, setPosts] = useState([])
@@ -20,16 +22,22 @@ function Posts() {
   const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(1)
 
+  const lastPageEl = useRef()
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data)
+    setPosts([...posts, ...response.data])
     const totalCount = response.headers['x-total-count']
     setTotalPages(getPageCount(totalCount, limit))
   })
 
+  useObserver(lastPageEl, page < totalPages, isPostsLoading, () => {
+    setPage(page + 1)
+  })
+
   useEffect(() => {
     fetchPosts(limit, page)
-  }, [])
+  }, [page, limit])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -42,7 +50,6 @@ function Posts() {
 
   const changePage = (p) => {
     setPage(p)
-    fetchPosts(limit, p)
   }
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
@@ -55,11 +62,21 @@ function Posts() {
       </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      <h3 style={{ marginTop: 7, fontSize: 16 }}
+      >Кількість елементів на сторінці</h3>
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        options={[
+          { value: 5, name: '5' },
+          { value: 10, name: '10' },
+          { value: 25, name: '25' },
+          { value: -1, name: 'Показати всі' }
+        ]} />
       {postError && <h1 style={{ fontSize: 50, textAlign: 'center', margin: '15px 0 15px 0' }} >Сталася помилка: {postError}</h1>}
-      {isPostsLoading
-        ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Пости про js' />
-      }
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Пости про js' />
+      <div ref={lastPageEl} style={{ height: 10, visibility: 'hidden' }}></div>
+      {isPostsLoading && <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>}
       <Pagination setFilter={setFilter} totalPages={totalPages} page={page} changePage={changePage} />
     </div >
   );
