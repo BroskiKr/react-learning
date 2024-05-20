7cafa3ddf07b502e2a9442317237ae1468930eb5
Andrew <krohmalnyj.andr@gmail.com>
2024-05-20 17:54:26 +0300

    try to make googleAuth

--- src/API/LoginService.js
+++ src/API/LoginService.js
@@ -5,4 +5,16 @@ export default class LoginService {
     const access_token = await axios.post('http://127.0.0.1:8000/login', credentials)
     return access_token
   }
+
+  static async loginGoogle() {
+    const response = await axios.get('http://127.0.0.1:8000/login/google')
+    const url = response.data.url
+    return url
+  }
+
+  static async getGoogleInfo(code) {
+    const response = await axios.get(`http://127.0.0.1:8000/auth/google?code=${code}`);
+    const userInfo = response.data;
+    return userInfo
+  }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,16 @@
 import MyButton from "../components/Ui/button/MyButton";
 import MyInput from "../components/Ui/input/MyInput";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../context";
 import LoginService from "../API/LoginService";
 import { getFormData } from "../utils/formData";
+import axios from "axios";
 
 const Login = (props) => {
   const { setIsAuth, setToken } = useContext(AuthContext);
   const [credentials, setCredentials] = useState({ username: '', password: '' })
   const [loginError, setLoginError] = useState(false)
-
-
+  const [url, setUrl] = useState('')
 
   const login = async (e) => {
     e.preventDefault();
@@ -33,6 +33,36 @@ const Login = (props) => {
     }
   }
 
+
+  useEffect(() => {
+    async function getUrl() {
+      setUrl(await LoginService.loginGoogle())
+    }
+    getUrl()
+  }, [])
+
+
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get('code');
+
+      if (code) {
+        try {
+          const userInfo = await LoginService.getGoogleInfo(code)
+          console.log(userInfo)
+          if (userInfo) {
+            setIsAuth(true)
+          }
+        } catch (error) {
+          console.error('Error fetching access token:', error);
+        }
+      }
+    };
+
+    fetchAccessToken();
+  }, []);
+
   return (
     <div style={{ width: 400 }}>
       <h1 style={{ fontSize: 30, fontWeight: 'bold' }}>Вхід</h1>
@@ -42,7 +72,9 @@ const Login = (props) => {
         {loginError && <div style={{ color: 'red', fontSize: '16px', paddingBottom: 5 }}>Invalid credentials.Try again</div>}
         <MyButton onClick={(e) => login(e)}>Увійти</MyButton>
       </form>
-    </div>
+      <a href={url} style={{ fontSize: 18, fontWeight: 'bold', display: 'flex', marginTop: '7px', alignItems: 'center', width: 120, justifyContent: 'space-between', }}>Login with <img style={{ display: 'block', width: 25, height: 25 }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/480px-Google_%22G%22_logo.svg.png" alt="Опис картинки" />
+      </a>
+    </div >
   );
 }
 
